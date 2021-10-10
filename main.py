f47f376c543be99caa0b6cb46cde8c087b69f5e7
ObnoxiousShardz <85340687+ObnoxiousShardz@users.noreply.github.com>
2021-10-10 21:58:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ from RemoveEntry import Clearer
 from Writer import Writer
 from ViewTable import Table
 
-if __name__ == '__main__':
+print("======================================================================================================="
+      "\nWelcome to Drug Data Manager!\nYou can add, remove or fetch for drugs and their count with the help of this program.\n"
+      "=======================================================================================================")
+time.sleep(3)
+choice = int(
+    input("Available Options:\n1. Add New Entry (1)\n2. Remove An Entry (2)\n3. Fetch For Data (3)\n4. Delete All Records (4)\n5. See The Contents of the Table (5)\nYour Choice: "))
+print("=======================================================================================================")
+print("Processing Your Request to Server...")
+print("=======================================================================================================")
+time.sleep(1.5)
 
-    print("======================================================================================================="
-          "\nWelcome to Drug Data Manager!\nYou can add, remove or fetch for drugs and their count with the help of this program.\n"
-          "=======================================================================================================")
-    time.sleep(3)
-    choice = int(
-        input(
-            "Available Options:\n1. Add New Entry (1)\n2. Remove An Entry (2)\n3. Fetch For Data (3)\n4. Delete All Records (4)\n5. See The Contents of the Table (5)\nYour Choice: "))
-    print("=======================================================================================================")
-    print("Processing Your Request to Server...")
-    print("=======================================================================================================")
-    time.sleep(1.5)
-
-    if choice == 1:
-        Writer()
-    elif choice == 2:
-        Clearer()
-    elif choice == 3:
-        Fetcher()
-    elif choice == 4:
-        DeleteEverything()
-    elif choice == 5:
-        Table()
-    else:
-        print("No such option available!")
+if choice == 1:
+    Writer()
+elif choice == 2:
+    Clearer()
+elif choice == 3:
+    Fetcher()
+elif choice == 4:
+    DeleteEverything()
+elif choice == 5:
+    Table()
+else:
+    print("No such option available!")
+    print("Exiting in 5 seconds")
+    time.sleep(5)
